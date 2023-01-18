66f20011cc1c42dab9f5c42a6adf9ebe1d61336f
Juhye O <artigen3@gmail.com>
2023-01-18 12:59:36 +0900

    회원가입, 로그인 완료

--- src/main/java/shop/mtcoding/buyer/model/ProductRepository.java
+++ src/main/java/shop/mtcoding/buyer/model/ProductRepository.java
@@ -2,6 +2,8 @@ package shop.mtcoding.buyer.model;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface ProductRepository {
     public int insert(String name, int price, int qty);
 
@@ -9,7 +11,8 @@ public interface ProductRepository {
 
     public Product findById(int id);
 
-    public int updateById(int id, String name, int price, int qty);
+    public int updateById(@Param("id") int id, @Param("name") String name, @Param("price") int price,
+            @Param("qty") int qty);
 
     public int deleteById(int id);
 }
--- src/main/java/shop/mtcoding/buyer/model/PurchaseRepository.java
+++ src/main/java/shop/mtcoding/buyer/model/PurchaseRepository.java
@@ -2,14 +2,16 @@ package shop.mtcoding.buyer.model;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface PurchaseRepository {
-    public int insert(int userId, int productId);
+    public int insert(@Param("user_id") int userId, @Param("product_id") int productId);
 
     public List<Purchase> findAll();
 
     public Purchase findById(int id);
 
-    public int updateById(int id, int userId, int productId);
+    public int updateById(@Param("id") int id, @Param("user_id") int userId, @Param("product_id") int productId);
 
     public int deleteById(int id);
 }
